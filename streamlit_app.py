e09e1bfac5cacb62a9a511329b9a8e87a52d638b
Luiz Albuquerque Leão <luiz_leao@yahoo.com>
2026-04-26 17:54:22 -0300

    Fix smoothie order logic and remove hardcoded API call

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,11 @@ if ingredients_list:
 
     for fruit_choise in ingredients_list:
         ingredients_string += fruit_choise + ' '
+        st.subheader(fruit_choise + ' Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)  
+        sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
 
+  
     st.write(ingredients_string)
 
     my_insert_stmt = """INSERT INTO smoothies.public.orders(ingredients, name_on_order)
@@ -43,7 +47,4 @@ if ingredients_list:
 
         st.success(f"Your Smoothie is ordered, {name_on_order}", icon="✅")
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-
-sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
 
